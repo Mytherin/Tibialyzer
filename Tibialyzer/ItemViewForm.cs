4c4d58672e59b25a9d7a9ece4a73f9a9c492696d
Mytherin <mark.raasveldt@gmail.com>
2015-11-14 13:52:59 +0100

    Small GUI fix for short item names.

--- Tibialyzer/ItemViewForm.cs
+++ Tibialyzer/ItemViewForm.cs
@@ -54,87 +54,40 @@ namespace Tibialyzer {
         }
 
         private void InitializeComponent() {
-            this.itemPictureBox = new System.Windows.Forms.PictureBox();
-            this.itemName = new System.Windows.Forms.Label();
-            this.itemCategory = new System.Windows.Forms.Label();
-            this.lookText = new System.Windows.Forms.Label();
-            this.valueLabel = new System.Windows.Forms.Label();
-            this.pickupBox = new System.Windows.Forms.CheckBox();
-            this.convertBox = new System.Windows.Forms.CheckBox();
-            this.valueBox = new System.Windows.Forms.TextBox();
             this.statsButton = new System.Windows.Forms.Label();
+            this.valueBox = new System.Windows.Forms.TextBox();
+            this.convertBox = new System.Windows.Forms.CheckBox();
+            this.pickupBox = new System.Windows.Forms.CheckBox();
+            this.valueLabel = new System.Windows.Forms.Label();
+            this.lookText = new System.Windows.Forms.Label();
+            this.itemCategory = new System.Windows.Forms.Label();
+            this.itemName = new System.Windows.Forms.Label();
+            this.itemPictureBox = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.itemPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
-            // itemPictureBox
+            // statsButton
             // 
-            this.itemPictureBox.BackColor = System.Drawing.Color.Transparent;
-            this.itemPictureBox.Location = new System.Drawing.Point(66, 49);
-            this.itemPictureBox.Name = "itemPictureBox";
-            this.itemPictureBox.Size = new System.Drawing.Size(32, 32);
-            this.itemPictureBox.TabIndex = 0;
-            this.itemPictureBox.TabStop = false;
+            this.statsButton.BackColor = System.Drawing.Color.Transparent;
+            this.statsButton.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.statsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statsButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(191)))), ((int)(((byte)(191)))));
+            this.statsButton.Location = new System.Drawing.Point(34, 115);
+            this.statsButton.Name = "statsButton";
+            this.statsButton.Padding = new System.Windows.Forms.Padding(2);
+            this.statsButton.Size = new System.Drawing.Size(96, 21);
+            this.statsButton.TabIndex = 27;
+            this.statsButton.Text = "Drops";
+            this.statsButton.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.statsButton.Click += new System.EventHandler(this.statsButton_Click);
             // 
-            // itemName
+            // valueBox
             // 
-            this.itemName.BackColor = System.Drawing.Color.Transparent;
-            this.itemName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.itemName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(191)))), ((int)(((byte)(191)))));
-            this.itemName.Location = new System.Drawing.Point(34, 84);
-            this.itemName.Name = "itemName";
-            this.itemName.Size = new System.Drawing.Size(96, 16);
-            this.itemName.TabIndex = 1;
-            this.itemName.Text = "name";
-            this.itemName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // itemCategory
-            // 
-            this.itemCategory.AutoSize = true;
-            this.itemCategory.BackColor = System.Drawing.Color.Transparent;
-            this.itemCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.itemCategory.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(191)))), ((int)(((byte)(191)))));
-            this.itemCategory.Location = new System.Drawing.Point(50, 20);
-            this.itemCategory.Name = "itemCategory";
-            this.itemCategory.Size = new System.Drawing.Size(69, 16);
-            this.itemCategory.TabIndex = 2;
-            this.itemCategory.Text = "category";
-            // 
-            // lookText
-            // 
-            this.lookText.AutoSize = true;
-            this.lookText.BackColor = System.Drawing.Color.Transparent;
-            this.lookText.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(191)))), ((int)(((byte)(191)))));
-            this.lookText.Location = new System.Drawing.Point(141, 75);
-            this.lookText.MaximumSize = new System.Drawing.Size(210, 0);
-            this.lookText.Name = "lookText";
-            this.lookText.Size = new System.Drawing.Size(64, 13);
-            this.lookText.TabIndex = 3;
-            this.lookText.Text = "You see a...";
-            // 
-            // valueLabel
-            // 
-            this.valueLabel.AutoSize = true;
-            this.valueLabel.BackColor = System.Drawing.Color.Transparent;
-            this.valueLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.valueLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(191)))), ((int)(((byte)(191)))));
-            this.valueLabel.Location = new System.Drawing.Point(141, 13);
-            this.valueLabel.Name = "valueLabel";
-            this.valueLabel.Size = new System.Drawing.Size(56, 16);
-            this.valueLabel.TabIndex = 4;
-            this.valueLabel.Text = "Value: ";
-            // 
-            // pickupBox
-            // 
-            this.pickupBox.AutoSize = true;
-            this.pickupBox.BackColor = System.Drawing.Color.Transparent;
-            this.pickupBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(191)))), ((int)(((byte)(191)))));
-            this.pickupBox.Location = new System.Drawing.Point(133, 41);
-            this.pickupBox.Name = "pickupBox";
-            this.pickupBox.Size = new System.Drawing.Size(87, 17);
-            this.pickupBox.TabIndex = 5;
-            this.pickupBox.Text = "Pick Up Item";
-            this.pickupBox.UseVisualStyleBackColor = false;
-            this.pickupBox.CheckedChanged += new System.EventHandler(this.pickupBox_CheckedChanged);
+            this.valueBox.Location = new System.Drawing.Point(195, 12);
+            this.valueBox.Name = "valueBox";
+            this.valueBox.Size = new System.Drawing.Size(148, 20);
+            this.valueBox.TabIndex = 7;
+            this.valueBox.TextChanged += new System.EventHandler(this.valueBox_TextChanged);
             // 
             // convertBox
             // 
@@ -149,32 +102,79 @@ namespace Tibialyzer {
             this.convertBox.UseVisualStyleBackColor = false;
             this.convertBox.CheckedChanged += new System.EventHandler(this.convertBox_CheckedChanged);
             // 
-            // valueBox
+            // pickupBox
             // 
-            this.valueBox.Location = new System.Drawing.Point(195, 12);
-            this.valueBox.Name = "valueBox";
-            this.valueBox.Size = new System.Drawing.Size(148, 20);
-            this.valueBox.TabIndex = 7;
-            this.valueBox.TextChanged += new System.EventHandler(this.valueBox_TextChanged);
+            this.pickupBox.AutoSize = true;
+            this.pickupBox.BackColor = System.Drawing.Color.Transparent;
+            this.pickupBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(191)))), ((int)(((byte)(191)))));
+            this.pickupBox.Location = new System.Drawing.Point(133, 41);
+            this.pickupBox.Name = "pickupBox";
+            this.pickupBox.Size = new System.Drawing.Size(87, 17);
+            this.pickupBox.TabIndex = 5;
+            this.pickupBox.Text = "Pick Up Item";
+            this.pickupBox.UseVisualStyleBackColor = false;
+            this.pickupBox.CheckedChanged += new System.EventHandler(this.pickupBox_CheckedChanged);
             // 
-            // statsButton
+            // valueLabel
             // 
-            this.statsButton.BackColor = System.Drawing.Color.Transparent;
-            this.statsButton.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.statsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.statsButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(191)))), ((int)(((byte)(191)))));
-            this.statsButton.Location = new System.Drawing.Point(34, 110);
-            this.statsButton.Name = "statsButton";
-            this.statsButton.Padding = new System.Windows.Forms.Padding(2);
-            this.statsButton.Size = new System.Drawing.Size(96, 21);
-            this.statsButton.TabIndex = 27;
-            this.statsButton.Text = "Drops";
-            this.statsButton.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.statsButton.Click += new System.EventHandler(this.statsButton_Click);
+            this.valueLabel.AutoSize = true;
+            this.valueLabel.BackColor = System.Drawing.Color.Transparent;
+            this.valueLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.valueLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(191)))), ((int)(((byte)(191)))));
+            this.valueLabel.Location = new System.Drawing.Point(141, 13);
+            this.valueLabel.Name = "valueLabel";
+            this.valueLabel.Size = new System.Drawing.Size(56, 16);
+            this.valueLabel.TabIndex = 4;
+            this.valueLabel.Text = "Value: ";
+            // 
+            // lookText
+            // 
+            this.lookText.AutoSize = true;
+            this.lookText.BackColor = System.Drawing.Color.Transparent;
+            this.lookText.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(191)))), ((int)(((byte)(191)))));
+            this.lookText.Location = new System.Drawing.Point(141, 75);
+            this.lookText.MaximumSize = new System.Drawing.Size(210, 0);
+            this.lookText.Name = "lookText";
+            this.lookText.Size = new System.Drawing.Size(64, 13);
+            this.lookText.TabIndex = 3;
+            this.lookText.Text = "You see a...";
+            // 
+            // itemCategory
+            // 
+            this.itemCategory.AutoSize = true;
+            this.itemCategory.BackColor = System.Drawing.Color.Transparent;
+            this.itemCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.itemCategory.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(191)))), ((int)(((byte)(191)))));
+            this.itemCategory.Location = new System.Drawing.Point(50, 20);
+            this.itemCategory.Name = "itemCategory";
+            this.itemCategory.Size = new System.Drawing.Size(69, 16);
+            this.itemCategory.TabIndex = 2;
+            this.itemCategory.Text = "category";
+            // 
+            // itemName
+            // 
+            this.itemName.BackColor = System.Drawing.Color.Transparent;
+            this.itemName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.itemName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(191)))), ((int)(((byte)(191)))));
+            this.itemName.Location = new System.Drawing.Point(34, 84);
+            this.itemName.Name = "itemName";
+            this.itemName.Size = new System.Drawing.Size(96, 28);
+            this.itemName.TabIndex = 1;
+            this.itemName.Text = "name";
+            this.itemName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // itemPictureBox
+            // 
+            this.itemPictureBox.BackColor = System.Drawing.Color.Transparent;
+            this.itemPictureBox.Location = new System.Drawing.Point(66, 49);
+            this.itemPictureBox.Name = "itemPictureBox";
+            this.itemPictureBox.Size = new System.Drawing.Size(32, 32);
+            this.itemPictureBox.TabIndex = 0;
+            this.itemPictureBox.TabStop = false;
             // 
             // ItemViewForm
             // 
-            this.ClientSize = new System.Drawing.Size(378, 137);
+            this.ClientSize = new System.Drawing.Size(378, 151);
             this.Controls.Add(this.statsButton);
             this.Controls.Add(this.valueBox);
             this.Controls.Add(this.convertBox);
