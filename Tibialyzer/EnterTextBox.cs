9e054c4c7fa2b76e3db5fd1c055c85a7578063de
Mytherin <mark.raasveldt@gmail.com>
2015-11-15 20:34:48 +0100

    Added missing files.

---
+++ Tibialyzer/EnterTextBox.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Tibialyzer {
+    class EnterTextBox : TextBox {
+        protected override bool IsInputKey(Keys keyData) {
+            if (keyData == Keys.Return)
+                return true;
+            return base.IsInputKey(keyData);
+        }
+    }
+}
