8031b38516bfa0867b79e5ec5a37f683eb54c811
Mark Raasveldt <mark.raasveldt@gmail.com>
2016-01-13 20:22:03 +0100

    Added previous/next page to creature/item/NPC list display.

--- Tibialyzer/CreatureList.cs
+++ Tibialyzer/CreatureList.cs
@@ -13,7 +13,8 @@ namespace Tibialyzer {
         public string title = "List";
         public string prefix = "creature" + MainForm.commandSymbol;
 
-        public CreatureList() {
+        public CreatureList(int initialPage = 0) {
+            this.currentPage = initialPage;
             objects = null;
             InitializeComponent();
         }
@@ -48,24 +49,74 @@ namespace Tibialyzer {
 
         }
 
-        public override void LoadForm() {
-            this.SuspendForm();
-            foreach(TibiaObject obj in objects) {
-                disposableObjects.Add(obj);
+        private int currentPage = 0;
+        private List<Control> createdControls = new List<Control>();
+        private void refresh() {
+            foreach(Control c in createdControls) {
+                this.Controls.Remove(c);
+                c.Dispose();
             }
 
-            int base_y =  this.listTitle.Location.Y + this.listTitle.Height + 10;
-            int y = MainForm.DisplayCreatureList(this.Controls, objects, 10, base_y, 344, 4, true);
+            int base_y = this.listTitle.Location.Y + this.listTitle.Height + 10;
+            MainForm.PageInfo pageInfo = new MainForm.PageInfo(false, false);
+            int y = MainForm.DisplayCreatureList(this.Controls, objects, 10, base_y, 344, 4, true, null, 1, createdControls, currentPage, 600, pageInfo);
+            foreach (Control control in createdControls)
+                control.Click += openItemBox;
+
+            if (pageInfo.prevPage) {
+                PictureBox prevpage = new PictureBox();
+                prevpage.Location = new Point(10, base_y + y);
+                prevpage.Size = new Size(97, 23);
+                prevpage.Image = MainForm.prevpage_image;
+                prevpage.BackColor = Color.Transparent;
+                prevpage.SizeMode = PictureBoxSizeMode.StretchImage;
+                prevpage.Click += Prevpage_Click;
+                this.Controls.Add(prevpage);
+                createdControls.Add(prevpage);
+
+            }
+            if (pageInfo.nextPage) {
+                PictureBox nextpage = new PictureBox();
+                nextpage.Location = new Point(this.Size.Width - 108, base_y + y);
+                nextpage.Size = new Size(98, 23);
+                nextpage.BackColor = Color.Transparent;
+                nextpage.Image = MainForm.nextpage_image;
+                nextpage.SizeMode = PictureBoxSizeMode.StretchImage;
+                nextpage.Click += Nextpage_Click;
+                this.Controls.Add(nextpage);
+                createdControls.Add(nextpage);
+            }
+            if (pageInfo.prevPage || pageInfo.nextPage) {
+                y += 23;
+            }
+
+            this.Size = new Size(this.Size.Width, base_y + y + 10);
+        }
+
+        private void Nextpage_Click(object sender, EventArgs e) {
+            currentPage++;
+            this.SuspendForm();
+            refresh();
+            this.ResumeForm();
+        }
+
+        private void Prevpage_Click(object sender, EventArgs e) {
+            currentPage--;
+            this.SuspendForm();
+            refresh();
+            this.ResumeForm();
+        }
+
+        public override void LoadForm() {
+            this.SuspendForm();
+
+            this.NotificationInitialize();
+
+            refresh();
 
             this.listTitle.Text = title;
             this.listTitle.Location = new Point(this.Size.Width / 2 - this.listTitle.Width / 2, this.listTitle.Location.Y);
             
-            this.Size = new Size(this.Size.Width, base_y + y + 10);
-
-            foreach (Control control in this.Controls)
-                if (control is PictureBox)
-                    control.Click += openItemBox;
-            this.NotificationInitialize();
             this.NotificationFinalize();
             this.ResumeForm();
         }
