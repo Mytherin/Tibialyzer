e2fccb92d347c3d74ca081bd1bdc9d1981a599f4
Mark Raasveldt <mark.raasveldt@gmail.com>
2016-01-23 19:35:38 +0100

    Added killing in the name of task information to database.

---
+++ Database Scan/addkillinginthenameof.py
@@ -0,0 +1,69 @@
+
+# Copyright 2016 Mark Raasveldt
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import xml.etree.ElementTree
+import sqlite3
+
+database_file = 'database.db'
+#database_file = 'E:\\Github Projects\\Tibialyzer\\Tibialyzer\\Database\\database.db'
+cities_xmlfile = "Extra Information/killinginthenameof.xml"
+
+from coordinates import convert_x, convert_y
+
+conn = sqlite3.connect(database_file)
+c = conn.cursor()
+
+
+# cities
+c.execute('DROP TABLE IF EXISTS TaskGroups');
+c.execute('DROP TABLE IF EXISTS Tasks');
+c.execute('DROP TABLE IF EXISTS TaskCreatures');
+c.execute('CREATE TABLE TaskGroups(id INTEGER PRIMARY KEY AUTOINCREMENT, name STRING)')
+c.execute('CREATE TABLE Tasks(id INTEGER PRIMARY KEY AUTOINCREMENT, groupid INTEGER, count INTEGER, taskpoints INTEGER, bossid INTEGER, bossx INTEGER, bossy INTEGER, bossz INTEGER)')
+c.execute('CREATE TABLE TaskCreatures(taskid INTEGER, creatureid INTEGER)')
+root = xml.etree.ElementTree.parse(cities_xmlfile).getroot()
+for child in root.getchildren():
+    taskbracketname = child.find('Name').text
+    c.execute('INSERT INTO TaskGroups(name) VALUES (?)', (taskbracketname,))
+    bracketid = c.lastrowid
+    tasks = child.find('Tasks')
+    for task in tasks.getchildren():
+        creatures = task.find('Creature').text
+        count = int(task.find('Count').text)
+        taskpoints =  int(task.find('Points').text) if task.find('Points') != None else None
+        boss = task.find('Boss').text.strip().lower() if task.find('Boss') != None else None
+        bossid = None
+        if boss != None:
+            c.execute('SELECT id FROM Creatures WHERE LOWER(name)=? OR LOWER(title)=?', (boss, boss))
+            bossid = c.fetchall()[0][0]
+        uloc = task.find('BossLocation').text.split(',') if task.find('BossLocation') != None else (None, None, None)
+        if uloc[0] != None:
+            uloc[0] = convert_x(uloc[0])
+            uloc[1] = convert_y(uloc[1])
+            uloc[2] = int(uloc[2])
+        c.execute('INSERT INTO Tasks(groupid, count, taskpoints, bossid, bossx, bossy, bossz) VALUES (?,?,?,?,?,?,?)', (bracketid, count, taskpoints, bossid, uloc[0], uloc[1], uloc[2]))
+        taskid = c.lastrowid
+        for creature in creatures.split(';'):
+            creature = creature.strip().lower()
+            print(creature)
+            c.execute('SELECT id FROM Creatures WHERE LOWER(name)=? OR LOWER(title)=?', (creature, creature))
+            creatureid = c.fetchall()[0][0]
+            c.execute('INSERT INTO TaskCreatures(taskid, creatureid) VALUES (?,?)', (taskid, creatureid))
+
+conn.commit()
+
+
+
+
+
