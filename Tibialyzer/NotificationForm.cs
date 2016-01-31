3dbf76b306b3d9841fc536621692872b2c5fa0b0
Mark Raasveldt <mark.raasveldt@gmail.com>
2016-01-31 14:01:11 +0100

    Improved loading speed of notifications.

--- Tibialyzer/NotificationForm.cs
+++ Tibialyzer/NotificationForm.cs
@@ -162,13 +162,14 @@ namespace Tibialyzer {
 
         static int WS_EX_NOACTIVATE = 0x08000000;
         static int WS_EX_TOOLWINDOW = 0x00000080;
+        static int WS_EX_COMPOSITED = 0x02000000;
 
         protected override CreateParams CreateParams {
             get {
                 CreateParams baseParams = base.CreateParams;
 
                 baseParams.ExStyle |= (int)(
-                  WS_EX_NOACTIVATE | WS_EX_TOOLWINDOW);
+                  WS_EX_NOACTIVATE | WS_EX_TOOLWINDOW | WS_EX_COMPOSITED);
 
                 return baseParams;
             }
