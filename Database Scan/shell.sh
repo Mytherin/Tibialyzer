ffc9cc922d9c2054e98ddd1197b1b60658d0aa4b
Mark Raasveldt <mark.raasveldt@gmail.com>
2016-12-07 11:42:58 +0100

    Added some missing files/tasks and added new outfits/mounts.

---
+++ Database Scan/shell.sh
@@ -0,0 +1,10 @@
+
+
+python3 parseXML.py
+python3 addrashidinformation.py
+python3 addmapinformation.py
+python3 addcityinformation.py
+python3 addeventinformation.py
+python3 addkillinginthenameof.py
+python3 addhelpinformation.py
+python3 addextrainformation.py
